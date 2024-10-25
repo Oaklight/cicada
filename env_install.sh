1c4bfe3b36f2451a168eec09bc5f7c9bc92300f2
Peng Ding <pding.dp@foxmail.com>
2024-10-25 13:57:02 -0500

    update setup script

--- env_install.sh
+++ env_install.sh
@@ -1,9 +1,17 @@
 # setup MiniGPT-3D
 cd MiniGPT-3D
-conda env create -f environment.yml
+
+# Use mamba if present, otherwise use conda
+if command -v mamba &>/dev/null; then
+    mamba env create -f environment.yml
+else
+    conda env create -f environment.yml
+fi
+
 conda activate minigpt_3d
 bash env_install.sh
 
+cd ..
 # download MiniGPT-3D weight from huggingface or hf-mirror.com
 # more on how to use hf-mirror.com: https://hf-mirror.com
-./tools/hfd.sh YuanTang96/MiniGPT-3D --tool aria2c -x 16 --local-dir ./ --exclude config.json --exclude README.md --exclude .gitattributes
+./tools/hfd.sh YuanTang96/MiniGPT-3D --tool aria2c -x 16 --local-dir ./MiniGPT-3D --exclude config.json --exclude README.md --exclude .gitattributes

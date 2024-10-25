473ef7de040a7029fb3bc78e6466990fada12517
Peng Ding <pding.dp@foxmail.com>
2024-10-24 23:59:00 -0500

    add default color to the converted obj

--- to_pointcloud/convert.py
+++ to_pointcloud/convert.py
@@ -39,9 +39,27 @@ def step2obj(step_path: str, out_path: str):
 def write_ply(points, filename, text=False):
     """input: Nx3, write points to filename as PLY format."""
     points = [
-        (points[i, 0], points[i, 1], points[i, 2]) for i in range(points.shape[0])
+        (
+            points[i, 0],
+            points[i, 1],
+            points[i, 2],
+            254,
+            254,
+            254,
+        )  # give default white color (254, 254, 254) to all points
+        for i in range(points.shape[0])
     ]
-    vertex = np.array(points, dtype=[("x", "f4"), ("y", "f4"), ("z", "f4")])
+    vertex = np.array(
+        points,
+        dtype=[
+            ("x", "f4"),
+            ("y", "f4"),
+            ("z", "f4"),
+            ("red", "u1"),
+            ("green", "u1"),
+            ("blue", "u1"),
+        ],
+    )
     el = PlyElement.describe(vertex, "vertex", comments=["vertices"])
     with open(filename, mode="wb") as f:
         PlyData([el], text=text).write(f)
